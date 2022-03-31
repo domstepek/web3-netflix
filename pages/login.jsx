ea9c3a3a8c20c536f1b9f6a17a09149fe7591859
Dom Stepek <domstepek@gmail.com>
2022-03-31 09:36:43 -0700

    renaming files + scss

---
+++ pages/login.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import {} from 'web3uikit';
+
+export default function Login() {
+  return (
+    <div className="bg-zinc-800">
+      <Head>
+        <title>N3tflix - Login</title>
+        <meta name="description" content="Netflix built with Web3 Technology" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
